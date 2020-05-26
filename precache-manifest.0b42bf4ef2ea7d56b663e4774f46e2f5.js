6248e5fd83d8a51d8c1a3a8e0cfd17a1559be051
Koexygen <rexarkoexygen@gmail.com>
2020-05-26 16:54:22 +0400

    Updates

---
+++ precache-manifest.0b42bf4ef2ea7d56b663e4774f46e2f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c53d1b5ef30311dfab0e2f69fbcfe13",
+    "url": "/terminal/index.html"
+  },
+  {
+    "revision": "53f1fc8b630c769fb1f5",
+    "url": "/terminal/static/css/main.0483a4cb.chunk.css"
+  },
+  {
+    "revision": "cea0243c17efcf51ba77",
+    "url": "/terminal/static/js/2.198acf08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/terminal/static/js/2.198acf08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f1fc8b630c769fb1f5",
+    "url": "/terminal/static/js/main.7f9e9316.chunk.js"
+  },
+  {
+    "revision": "d5b61594c85035f86fcf",
+    "url": "/terminal/static/js/runtime-main.06ffbf07.js"
+  }
+]);
