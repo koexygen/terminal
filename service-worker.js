bf069121dd61522ce6334fd8c7689a86861647de
Koexygen <rexarkoexygen@gmail.com>
2020-12-02 09:21:47 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd4a22726c93692b36cc5bbb76609216.js"
+  "/precache-manifest.3b124d1bb49a736b5e137efc8fc715c4.js"
 );
 
 self.addEventListener('message', (event) => {
